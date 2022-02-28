699cb6ae4f33103e81aab6c6dd82bf2c4abcf55b
mahimakrovvidy <mkrovvidy@gmail.com>
2022-02-27 19:53:53 -0800

    create task

--- templates/Personal/createtaskmnn.py
+++ templates/Personal/createtaskmnn.py
@@ -6,23 +6,24 @@ age = int(input ("Enter your age!: "))
 
 vacaylist = ['New York City', 'Tokyo', 'Los Angeles', 'Paris', 'San Diego', 'Hawaii', 'Sedona', 'Amsterdam', 'Seoul'  ]
 
-if (pt) == "A":
-    if (age < 30):
-     print (vacaylist[0], vacaylist[2])
-    else:
-     print(vacaylist[1])
+def sortVacation (pt, age):
+    if (pt) == "A":
+        if (age < 30):
+            print (vacaylist[0], vacaylist[2])
+        else:
+            print(vacaylist[1])
 
-if (pt) == "B":
-    if (age < 30):
-        print(vacaylist[4], vacaylist[5])
-    else:
-        print(vacaylist[3])
+    if (pt) == "B":
+         if (age < 30):
+            print(vacaylist[4], vacaylist[5])
+         else:
+             print(vacaylist[3])
 
-elif (pt) == "C":
-    if (age < 30):
-        print (vacaylist[7], vacaylist[8])
-    else:
-        print(vacaylist[6])
+    elif (pt) == "C":
+        if (age < 30):
+            print (vacaylist[7], vacaylist[8])
+        else:
+            print(vacaylist[6])
 
 
 
